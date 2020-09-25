6194fae0612eba774f2744ddc3f7d5e9f7f55ad3
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-09-25 08:57:27 +0300

    ex00

---
+++ day01/ex00/test.swift
@@ -0,0 +1,10 @@
+var arrColor : [Color] = Color.allColors
+var arrValue : [Value] = Value.allValues
+
+for elem in arrColor {
+	print("\(elem) = \(elem.rawValue)")
+}
+
+for elem in arrValue {
+	print("\(elem) = \(elem.rawValue)")
+}
